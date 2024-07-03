cdbea52b250d379d730849f341d5c018d96c5478
Yevhen Martyn <Yevhen.Martyn@ventionteams.com>
2024-07-03 16:27:49 +0200

    basic CRUD added

--- EmployeeManagementSystem/WebAPI/Controllers/DepartmentController.cs
+++ EmployeeManagementSystem/WebAPI/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -23,10 +24,42 @@ namespace WebAPI.Controllers
         }
 
         // POST action
-
+        [HttpPost]
+        public IActionResult AddDepartment(Department department)
+        {
+            Services.DataService.AddDepartment(department);
+            return CreatedAtAction(nameof(GetDepartmentById), new { id = department.Id }, department);
+        }
 
         // PUT action
+        [HttpPut("{id}")]
+        public IActionResult UpdateDepartment(int id, Department department)
+        {
+            if (id != department.Id)
+                return BadRequest();
+
+            Department existingDepartment = Services.DataService.GetDepartmentById(id);
+            if (existingDepartment is null)
+                return NotFound();
+
+            Services.DataService.UpdateDepartment(department);
+
+            return Ok();
+        }
+
 
         // DELETE action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var department = Services.DataService.GetDepartmentById(id);
+
+            if (department is null)
+                return NotFound();
+
+            Services.DataService.DeleteDepartmentById(id);
+
+            return Ok();
+        }
     }
 }
--- EmployeeManagementSystem/WebAPI/Controllers/EmployeeController.cs
+++ EmployeeManagementSystem/WebAPI/Controllers/EmployeeController.cs
@@ -24,10 +24,42 @@ namespace WebAPI.Controllers
         }
 
         // POST action
-
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            Services.DataService.AddEmployee(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
 
         // PUT action
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployee(int id, Employee employee)
+        {
+            if (id != employee.Id)
+                return BadRequest();
+
+            Employee existingEmployee = Services.DataService.GetEmployeeById(id);
+            if (existingEmployee is null)
+                return NotFound();
+
+            Services.DataService.UpdateEmployee(employee);
+
+            return Ok();
+        }
+
 
         // DELETE action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var employee = Services.DataService.GetEmployeeById(id);
+
+            if (employee is null)
+                return NotFound();
+
+            Services.DataService.DeleteEmployeeById(id);
+
+            return Ok();
+        }
     }
 }
