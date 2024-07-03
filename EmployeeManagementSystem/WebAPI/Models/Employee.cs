bc3d94116478dd86ae7cc0531426967b681f201a
Yevhen Martyn <Yevhen.Martyn@ventionteams.com>
2024-07-03 12:57:37 +0200

    Employee model and dataService created

---
+++ EmployeeManagementSystem/WebAPI/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Position { get; set; }
+        public Department Department { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+
+    }
+}
---
+++ EmployeeManagementSystem/WebAPI/Services/DataService.cs
@@ -0,0 +1,44 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public static class DataService
+    {
+        static List<Employee> Employees { get; set; }
+        static int nextEmployeeId = 3;
+
+        static DataService()
+        {
+            Employees = new List<Employee> { new Employee {Name = "employee1" }, 
+                                             new Employee {Name = "employee2" },
+                                             new Employee {Name = "employee3" } };
+        }
+
+        //Employees
+        public static List<Employee> GetAllEmployees(int id) => Employees;
+        public static Employee GetEmployeeById(int id) => Employees.FirstOrDefault(employee => employee.Id == id);
+
+        public static void AddEmployee(Employee employee) {
+            employee.Id = nextEmployeeId;
+            nextEmployeeId++;
+            Employees.Add(employee);
+        }
+
+        public static void DeleteEmployeeById(int id) {
+            var employee = GetEmployeeById(id);
+            if (employee is null)
+                return;
+
+            Employees.Remove(employee);
+        }
+
+        public static void UpdateEmployee(Employee employee) {
+            int index = Employees.FindIndex(e => e.Id == employee.Id);
+            if (index == -1)
+                return;
+
+            Employees[index] = employee;
+        }
+
+    }
+}
