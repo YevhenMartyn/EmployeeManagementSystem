eb7a3e8b11de60dc2c3f0584f18562614549abd9
Yevhen Martyn <Yevhen.Martyn@ventionteams.com>
2024-07-03 13:19:01 +0200

    Department model and DataService added

--- EmployeeManagementSystem/WebAPI/Models/Department.cs
+++ EmployeeManagementSystem/WebAPI/Models/Department.cs
@@ -1,6 +1,11 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
 {
     public class Department
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
     }
 }
--- EmployeeManagementSystem/WebAPI/Services/DataService.cs
+++ EmployeeManagementSystem/WebAPI/Services/DataService.cs
@@ -5,16 +5,24 @@ namespace WebAPI.Services
     public static class DataService
     {
         static List<Employee> Employees { get; set; }
-        static int nextEmployeeId = 3;
+        static int nextEmployeeId = 4;
+
+        static List<Department> Departments { get; set; }
+        static int nextDepartmentId = 3;
 
         static DataService()
         {
-            Employees = new List<Employee> { new Employee {Name = "employee1" }, 
-                                             new Employee {Name = "employee2" },
-                                             new Employee {Name = "employee3" } };
+            Departments = new List<Department> { new Department {Id = 1, Name = "department1" },
+                                                 new Department {Id = 2, Name = "department2" } };
+
+
+            Employees = new List<Employee> { new Employee {Id = 1, Name = "employee1", Position = "position1", Department = GetDepartmentById(1), StartDate = new DateTime(2023, 4, 7) }, 
+                                             new Employee {Id = 2, Name = "employee2", Position = "position2", Department = GetDepartmentById(2), StartDate = new DateTime(2023, 7, 15) },
+                                             new Employee {Id = 3, Name = "employee3", Position = "position3", Department = GetDepartmentById(2), StartDate = new DateTime(2023, 9, 18) } };
         }
 
-        //Employees
+        //Employees ----------------
+
         public static List<Employee> GetAllEmployees(int id) => Employees;
         public static Employee GetEmployeeById(int id) => Employees.FirstOrDefault(employee => employee.Id == id);
 
@@ -40,5 +48,34 @@ namespace WebAPI.Services
             Employees[index] = employee;
         }
 
+        //Departments ------------------
+
+        public static List<Department> GetAllDepartments(int id) => Departments;
+        public static Department GetDepartmentById(int id) => Departments.FirstOrDefault(department => department.Id == id);
+
+        public static void AddDepartment(Department department)
+        {
+            department.Id = nextDepartmentId;
+            nextDepartmentId++;
+            Departments.Add(department);
+        }
+
+        public static void DeleteDepartmentById(int id)
+        {
+            var department = GetDepartmentById(id);
+            if (department is null)
+                return;
+
+            Departments.Remove(department);
+        }
+
+        public static void UpdateDepartment(Department department)
+        {
+            int index = Departments.FindIndex(d => d.Id == department.Id);
+            if (index == -1)
+                return;
+
+            Departments[index] = department;
+        }
     }
 }
