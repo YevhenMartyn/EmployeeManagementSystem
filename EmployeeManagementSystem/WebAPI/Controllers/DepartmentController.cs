d2b7a5bd3117afae5e971ea30455dc063e25a547
Yevhen Martyn <Yevhen.Martyn@ventionteams.com>
2024-07-03 14:36:47 +0200

    GET for employee and department

---
+++ EmployeeManagementSystem/WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        // GET all action
+        [HttpGet]
+        public IActionResult GetAllDepartments() => Ok(Services.DataService.GetAllDepartments());
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public IActionResult GetDepartmentById(int id)
+        {
+            var department = Services.DataService.GetDepartmentById(id);
+            if (department == null)
+                return NotFound();
+
+            return Ok(department);
+        }
+
+        // POST action
+
+
+        // PUT action
+
+        // DELETE action
+    }
+}
---
+++ EmployeeManagementSystem/WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET all action
+        [HttpGet]
+        public IActionResult GetAllEmployees() => Ok(Services.DataService.GetAllEmployees());
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            var employee = Services.DataService.GetEmployeeById(id);
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        // POST action
+
+
+        // PUT action
+
+        // DELETE action
+    }
+}
