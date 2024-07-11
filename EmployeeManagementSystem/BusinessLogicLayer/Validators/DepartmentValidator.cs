c07ba93623ca6285d98a6bca3a614b35dd194d85
Yevhen Martyn <Yevhen.Martyn@ventionteams.com>
2024-07-11 10:00:41 +0200

    FluentValidation added(not used yet)

---
+++ EmployeeManagementSystem/BusinessLogicLayer/Validators/DepartmentValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using BusinessLogicLayer.Models;
+
+namespace BusinessLogicLayer.Validators
+{
+    public class DepartmentValidator : AbstractValidator<DepartmentModel>
+    {
+        public DepartmentValidator()
+        {
+            RuleFor(d => d.Name).NotEmpty().WithMessage("Name is required.")
+                                .MaximumLength(30).WithMessage("Name cannot exceed 30 characters.");
+        }
+    }
+}
---
+++ EmployeeManagementSystem/BusinessLogicLayer/Validators/EmployeeValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using BusinessLogicLayer.Models;
+
+namespace BusinessLogicLayer.Validators
+{
+    public class EmployeeValidator : AbstractValidator<EmployeeModel>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(e => e.Name).NotEmpty().WithMessage("Name is required.")
+                                 .MaximumLength(30).WithMessage("Name cannot exceed 30 characters.");
+
+            RuleFor(e => e.Position).NotEmpty().WithMessage("Position is required.")
+                                    .MaximumLength(30).WithMessage("Position cannot exceed 30 characters.");
+
+            RuleFor(e => e.DepartmentId).GreaterThan(-1).WithMessage("Invalid DepartmentId.");
+
+            RuleFor(e => e.StartDate).NotEmpty().WithMessage("Start Date is required.");
+        }
+    }
+}
