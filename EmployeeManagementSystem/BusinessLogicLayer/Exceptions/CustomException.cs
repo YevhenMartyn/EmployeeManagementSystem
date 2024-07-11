1cf12d3ad6c033f95200bb6d224d3d2a6b31a7e9
Yevhen Martyn <Yevhen.Martyn@ventionteams.com>
2024-07-11 14:47:07 +0200

    Custom error for HTTP codes storage

---
+++ EmployeeManagementSystem/BusinessLogicLayer/Exceptions/CustomException.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace BusinessLogicLayer.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public CustomException(string message, HttpStatusCode statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
